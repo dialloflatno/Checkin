2e5fe450cd248ee3491792ad7b6d7da026025221
Nick Bechtel <bechtel.nick@gmail.com>
2022-01-04 11:37:39 -0500

    Authorize for username login added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    before_action :authorize
   def create
     user = User.find_by(username: params[:username])
     session[:user_id] = user.id
@@ -8,5 +9,11 @@ class SessionsController < ApplicationController
   def destroy
     session.delete :user_id
     head :no_content
+  end
+
+  private
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 end
--- db/migrate/20211226042145_create_users.rb
+++ db/migrate/20211226042145_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :first_name 
-      t.string :last_name 
-      t.string :email 
-      t.integer :phone_number 
-      t.string :user_name 
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
+      t.string :username
       t.string :password
 
       t.timestamps
