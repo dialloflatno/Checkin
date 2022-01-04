2260c8d78ef7e58446a24654aea74d0dcddafa74
Nick Bechtel <bechtel.nick@gmail.com>
2022-01-04 16:48:48 -0500

    most recent backup 4:48Pm

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,10 +7,3 @@ class Event < ApplicationRecord
   validates :date, presence: true, numericality: { only_integer: true }
   validates :location_id, presence: true
 end
-Event.create(
-  title: 'RACK CATS VS NEW CHARTER BASKET BALL',
-  planner: 'BobCat Club',
-  time: Time.new(2022, 1, 1, 0, 9, 0, 0),
-  date: DateTime.new(2022, 1, 1, 0, 0, 0),
-  location_id: 4,
-)
