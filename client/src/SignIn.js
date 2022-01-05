31b54bd6f3c0089b206f2bdeb426e63a57db08e2
Nick Bechtel <bechtel.nick@gmail.com>
2022-01-05 15:57:58 -0500

    Fixed username syntax errors across our code + established signup and signin functionalities

--- client/src/SignIn.js
+++ client/src/SignIn.js
@@ -1,42 +1,55 @@
 import { useState } from "react";
 
 function SignIn({ setUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [user_name, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
 
-    function handleSubmit (e){
-        e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }
 
-        fetch('/login', {
-            method:'POST',
-            headers: { 
-                "Content-Type" : 'application/json',
-            },
-            body: JSON.stringify({ user_name ,password}),
-        }).then((r) => {
-            if(r.ok) {
-                r.json().then((user) => setUser(user));
-            }
-          })  
-        }
+  // const handleChange = (e) => {
+  //     console.log(e.target.value)
+  // }
 
-    // const handleChange = (e) => {
-    //     console.log(e.target.value)
-    // }
-
-    return (
-        <div className="signContainer">
-            <form onSubmit = {handleSubmit}>
-            <h1 className='logo'>Checkedin</h1>
-            <input className ='username' type='text' value={user_name} onChange = { (e) => setUsername(e.target.value)}placeholder ='username'/>
-            <br/>
-            <input className ='password' type='password' value={password} onChange = { (e) =>  setPassword(e.target.value)} placeholder ='password'/>
-            <button className = 'signIn' type ='submit' >Sign In</button>
-            </form>
-            <h2>Your not Checkedin Yet ?</h2>
-        </div>
-        );
+  return (
+    <div className="signContainer">
+      <form onSubmit={handleSubmit}>
+        <h1 className="logo">Checkedin</h1>
+        <input
+          className="username"
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="username"
+        />
+        <br />
+        <input
+          className="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+        />
+        <button className="signIn" type="submit">
+          Sign In
+        </button>
+      </form>
+      <h2>Your not Checkedin Yet ?</h2>
+    </div>
+  );
 }
 
 export default SignIn;
