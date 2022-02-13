f3bc895e441f143a41770a47ec8631ad7f707a4c
Ibrahima Diallo <91293413+dialloflatno@users.noreply.github.com>
2022-02-12 19:59:30 -0500

    Heyh (#64)
    * Clock added bckg changed
    
    * nav static
    
    * nav static 2
    
    * avgBall
    
    * avgBall2
    
    * avg added
    
    * subject is nested
    
    * names are showing in onbe array
    
    * locations show but no teaacher name ore start end  time
    
    * home page displays
    
    * actions controller created
    
    * custom routes created
    
    * admin routes setup have to do the destroy methods
    
    * routes moved to Appjs
    
    * favicon added
    
    * grades page changed;
    
    * can login in as to different users but the admin is not showing in the user object

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ import { Route, Switch, Redirect } from 'react-router-dom'
 import Login from './Login'
 import SignUp from './SignUp.js'
 import NavBar from './NavBar'
-import Errorpage from './ErrorPage'
 import Home from './Home'
 import Emergency from './Emergency'
 import Schedule from './Schedule'
 import Profile from './Profile'
 import Grades from './Grades'
 import Events from './Events'
+import Admin from './Admin'
 
 function App() {
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState([])
+
 
   useEffect(() => {
     fetch('/me').then((r) => {
@@ -27,51 +28,68 @@ function App() {
     setUser(user)
   }
 
-  return (
-    <>
+
+  if (user.student) {
+    return (
       <main>
-        {user ? (
-          <Switch>
-            <Route exact path="/">
-              <NavBar user={user} setUser={setUser} />
-              <Home user={user} />
-            </Route>
-            <Route path="/emergency">
-              <NavBar user={user} setUser={setUser} />
-              <Emergency />
-            </Route>
-            <Route path="/schedule">
-              <NavBar user={user} setUser={setUser} />
-              <Schedule />
-            </Route>
-            <Route path="/profile">
-              <NavBar user={user} setUser={setUser} />
-              <Profile />
-            </Route>
-            <Route path="/grades">
-              <NavBar user={user} setUser={setUser} />
-              <Grades />
-            </Route>
-            <Route exact path="/events">
-              <NavBar user={user} setUser={setUser} />
-              <Events />
-            </Route>
-          </Switch>
-        ) : (
-          <Switch>
-            <Route path="/signup">
-              <SignUp user={user} setUser={setUser} />
-            </Route>
-            <Route>
-              <Login handleLogin={handleLogin} />
-            </Route>
-            <Redirect to="/login" />
-            <Route path="/home"></Route>
-          </Switch>
-        )}
+        <Switch>
+          <Route exact path="/">
+            <NavBar user={user} setUser={setUser} />
+            <Home user={user} />
+          </Route>
+          <Route path="/emergency">
+            <NavBar user={user} setUser={setUser} />
+            <Emergency />
+          </Route>
+          <Route path="/schedule">
+            <NavBar user={user} setUser={setUser} />
+            <Schedule />
+          </Route>
+          <Route path="/profile">
+            <NavBar user={user} setUser={setUser} />
+            <Profile />
+          </Route>
+          <Route path="/grades">
+            <NavBar user={user} setUser={setUser} />
+            <Grades />
+          </Route>
+          <Route exact path="/events">
+            <NavBar user={user} setUser={setUser} />
+            <Events />
+          </Route>
+        </Switch>
       </main>
-    </>
-  )
+
+    )
+  } else if ( user.teacher === true ) {
+
+    return (
+      <>
+        <Switch>
+          <Route path='/'>
+            <Admin />
+          </Route>
+        </Switch>
+      </>
+    )
+  }
+
+  else {
+    return (
+      <>
+        <Switch>
+          <Route path="/signup">
+            <SignUp user={user} setUser={setUser} />
+          </Route>
+          <Route>
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Redirect to="/login" />
+          <Route path="/home"></Route>
+        </Switch>
+      </>
+    )
+  }
 }
 
 export default App
