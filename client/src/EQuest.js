88ea1bddc668948442bd9596203a2e58d7f92296
Nick Bechtel <87031638+nikudon612@users.noreply.github.com>
2022-05-18 14:10:37 -0400

    Teacher view of requests displays item in table format. But still inside cards. Needs reworking of React

--- client/src/EQuest.js
+++ client/src/EQuest.js
@@ -1,11 +1,30 @@
 export default function EQuest({ emerg, location, studentID }) {
   return (
     <div id="eqstack">
-      <span>Emergency: {emerg}</span>
+      {/* OLD VIEW OF EMERGENCIES AS CARDS */}
+      {/* <span>Emergency: {emerg}</span>
       <br />
       <span>Room: {location}</span>
       <br />
-      <span>Student#ID: {studentID}</span>
+      <span>Student#ID: {studentID}</span> */}
+
+      {/* NEW VIEW OF EMERGENCIES AS A TABLE */}
+      <table>
+        <thead>
+          <tr>
+            <th>Emergency</th>
+            <th>Location</th>
+            <th>Student ID#</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{emerg}</td>
+            <td>{location}</td>
+            <td>{studentID}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
