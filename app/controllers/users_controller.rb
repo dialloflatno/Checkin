b01d4f4f2e8f9bfb18ae129268f60e016a1acea3
dialloflatno <brahimdiallo23@gmail.com>
2022-05-24 16:11:52 -0400

    user credits are displayed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
+    byebug
     if user
       render json: user, status: :created
     else
--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,33 +1,49 @@
 import { useEffect, useState } from "react"
 import StudentUser from './StudentUser'
 import TeacherUser from './TeacherUser'
+import User from "./User"
 
 
 
 
 export default function Admin() {
 
-const [allUsers,setAllUsers ] = useState('')
+  const [allUsers, setAllUsers] = useState('')
 
-useEffect(() => {
+  useEffect(() => {
     fetch('/users').then((r) => {
       if (r.ok) {
         r.json().then((everyUser) => setAllUsers(everyUser))
       }
     })
   }, [])
-// debugger
+  // debugger
   console.log(allUsers);
 
-    return (
-        <>
-            <StudentUser students = { allUsers.students}/>
-            <TeacherUser teachers = {allUsers.teachers}/>
-            
 
-        </>
+  let columnUsers = "No Users";
 
+  if (allUsers.length) {
+    columnUsers = allUsers?.map((users) => (
+      <ul>
+       <li>{users. full_name} <br/> Email Address: {users.email} <br/> School:{users.school}<br/>Student ID NUMBER:{users.IDNUMBER}<br/>{users.state}<br/>{users.student}</li>
+      </ul>
     )
+    )
+  }
+  return (
+    <>
+
+      <div class='columnWork'>
+        {
+ columnUsers
+
+        }
+
+      </div>
+    </>
+
+  )
 
 
 }
