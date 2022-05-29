946b4942bb8aea965b5def07c198fbcee3f263a8
Ibrahima Diallo <91293413+dialloflatno@users.noreply.github.com>
2022-05-29 18:32:01 -0400

    Working pox2 (#85)
    * user credits are displayed
    
    * tabel is added
    
    * tabel corrected
    
    * updated route created
    
    * patch fixed
    
    * add patch structure
    
    * working
    
    * working on coulmns
    
    * toggles student and teacher
    
    * teacher working partially

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -1,31 +1,28 @@
-import { useEffect, useState } from "react"
-import StudentUser from './StudentUser'
-import TeacherUser from './TeacherUser'
-import User from "./User"
-
-
-
+import { useEffect, useState } from "react";
+import StudentUser from "./StudentUser";
+import TeacherUser from "./TeacherUser";
+import User from "./User";
 
 export default function Admin() {
-
-  const [allUsers, setAllUsers] = useState('')
-  const [teacher, setAsTeacher] = useState('')
-  const [student, setAsStudent] = useState(false)
+  
+  const [allUsers, setAllUsers] = useState("");
+  const [teacher, setAsTeacher] = useState(false);
+  const [student, setAsStudent] = useState(false);
 
   useEffect(() => {
-    fetch('/users').then((r) => {
+    fetch("/users").then((r) => {
       if (r.ok) {
-        r.json().then((everyUser) => setAllUsers(everyUser))
+        r.json().then((everyUser) => setAllUsers(everyUser));
       }
-    })
-  }, [])
+    });
+  }, []);
   // debugger
   console.log(allUsers);
 
-
   function handleClickStudent(id) {
     console.log(`student id is ${id}`);
-    setAsStudent((student) => !student)
+    setAsStudent((student) => !student);
+
     fetch(`/users/${id}`, {
       method: "PATCH",
       headers: {
@@ -37,23 +34,35 @@ export default function Admin() {
     })
       .then((r) => r.json())
       .then((data) => {
-        setAsTeacher(() => (!data));
+        setAsTeacher(() => !data);
       });
   }
 
-  function handleClickTeacher(e) {
-    console.log('teacher');
-    setAsTeacher((e) => true)
-
+  function handleClickTeacher(id) {
+    console.log(`teacher is now ${id}`);
+    setAsTeacher((teacher) => !teacher);
+    fetch(`/users/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        teacher: teacher,
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => {
+        setAsStudent(() => !data);
+      });
   }
-
-
 
   let columnUsers = "No Users";
 
   if (allUsers.length) {
     columnUsers = allUsers?.map((users) => (
-      <div class='userTabel'>
+      
+      <div class="userTabel">
+
         <>
           <tbody>
             <td>{users.full_name}</td>
@@ -61,19 +70,25 @@ export default function Admin() {
             <td>{users.school}</td>
             <td>{users.IDNUMBER}</td>
             <td>{users.state}</td>
-            <td>{users.student}<button onClick={() => handleClickStudent(users.id)}>Student</button> <button onClick={handleClickTeacher} >Teacher</button></td>
+            <td>
+              {users.student}
+              <button onClick={() => handleClickStudent(users.id)}>
+                Student
+              </button>{" "}
+              <button onClick={() => handleClickTeacher(users.id)}>
+                Teacher
+              </button>
+            </td>
           </tbody>
         </>
         )
       </div>
-    )
-    )
+    ));
   }
   return (
     <>
-
-      <div class='columnWork'>
-        <div class='userTabel'>
+      <div class="columnWork">
+        <div class="userTabel">
           <table>
             <thead>
               <tr>
@@ -90,5 +105,6 @@ export default function Admin() {
         </div>
       </div>
     </>
-  )
+  );
 }
+
